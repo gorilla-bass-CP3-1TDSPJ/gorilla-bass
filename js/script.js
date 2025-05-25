41e64e42c8ec16b567567753c7ad3d882a99ce51
manu <haru.noom0@gmail.com>
2025-05-25 20:13:51 -0300

    Funções de barras de vida e dano do gorila no JS

--- js/script.js
+++ js/script.js
@@ -12,9 +12,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnAtaque = document.getElementById("bt-ataque");
     const btnCura = document.getElementById("bt-cura");
     const btnDefesa = document.getElementById("bt-defesa");
-    
+
     let vidaGorila = 100;
     let vidaNinjas = 100;
     let defesaAtiva = false;
     let jogando = false;
+
+    function atualizarBarraVidaNinja() {
+        vidaNinjas = Math.max(0, Math.min(vidaNinjas, 100));
+        vidaTotalNinja.style.width = vidaNinjas + "%";
+    }
+    function atualizarBarraVidaGorila() {
+        vidaGorila = Math.max(0, Math.min(vidaGorila, 100));
+        vidaTotalGorila.style.width = vidaGorila + "%";
+    }
+    function aplicarDanoGorila() {
+        if (defesaAtiva) {
+            defesaAtiva = false;
+        } else {
+            vidaGorila -= 5;
+        }
+        atualizarBarraVidaGorila();
+    }
 });
